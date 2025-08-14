4f62726ef6862a7c4e24c4c81a94972b2f4d7251
Lucas ESPIET <lucas.espiet@atoptima.com>
2025-08-14 17:20:15 +0200

    feat(user): added user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -5,6 +5,21 @@ export default class User extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  declare email: string
+
+  @column()
+  declare password: string | null
+
+  @column()
+  declare mfaEnabled: boolean
+
+  @column()
+  declare mfaSecret: string | null
+
+  @column()
+  declare roleId: number | null
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
