8117c22b79d0424582103b64f332964e5fd82d70
Lucas ESPIET <lucas.espiet@atoptima.com>
2025-08-14 14:29:38 +0200

    implement new migration and models for auth ToDo!

---
+++ database/migrations/1755173822609_create_users_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string('email').unique()
+      table.string('password').nullable()
+      table.boolean('mfa_enabled').defaultTo(false)
+      table.string('mfa_secret').nullable()
+      table.integer('role_id').unsigned().nullable()
+      table.foreign('role_id').references('id').inTable('roles').onDelete('SET NULL')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
